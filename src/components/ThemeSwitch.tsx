1bd20a83304df3026df32cff6102f0f801b5a146
Drey-1 <matosandrey11@gmail.com>
2026-02-22 15:55:10 -0300

    refactor: rename toggleSwitch to ThemeSwitch for better clarity

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,26 +1,26 @@
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "@/hooks/useTheme";
 
-export default function ToggleSwitch() {
+export default function ThemeSwitch() {
 	const { toggleTheme, isDarkMode } = useTheme();
 
 	return (
-		<label className="relative inline-block w-18 h-9 group m-2">
+		<label className="relative inline-block w-20 h-10 group m-2 ">
 			<input type="checkbox" onChange={toggleTheme} checked={isDarkMode} className="sr-only" />
 
 			<div className="w-full h-full rounded-full border border-card ">
 				<div className="flex p-1 justify-between">
 					<Sun
-						size={26}
+						size={29}
 						className="z-10 text-icons group-has-[input:checked]:text-card-foreground	"
 					/>
 					<Moon
-						size={26}
+						size={29}
 						className="z-10 text-card-foreground group-has-[input:checked]:text-icons"
 					/>
 				</div>
 				<div
-					className={`absolute h-8 w-8  top-0.5 bg-foreground rounded-full transition  ${isDarkMode ? "right-0.5" : "left-0.5"}`}
+					className={`absolute h-9 w-9  top-0.5 bg-foreground rounded-full transition  ${isDarkMode ? "right-0.5" : "left-0.5"} group-hover:brightness-90 dark:group-hover:brightness-110`}
 				></div>
 			</div>
 		</label>
