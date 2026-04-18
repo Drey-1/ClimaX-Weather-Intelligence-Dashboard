12ccfb737fa95822d371a1e060a86adbf8107df2
Drey-1 <matosandrey11@gmail.com>
2026-04-18 16:47:27 -0300

    feat: implement dashboard route with data presentation toggle between simple and deep views

--- src/components/DeepSchedule.tsx
+++ src/components/DeepSchedule.tsx
@@ -1,64 +1,61 @@
-import { CloudDrizzleIcon, DropletIcon } from "lucide-react"
+import { CloudDrizzleIcon, DropletIcon } from "lucide-react";
 
 const mock = [
-    {
-        day: "01",
-        weekday: "Mon",
-        Icon: CloudDrizzleIcon,
-        minMax: ["23°C","28°C"],
-        precipitation: "19.88mm"
-
-    },
-    {
-        day: "02",
-        weekday: "Tue",
-        Icon: CloudDrizzleIcon,
-        minMax: ["23°C","28°C"],
-        precipitation: "19.88mm"
-
-    },
-    {
-        day: "03",
-        weekday: "Wed",
-        Icon: CloudDrizzleIcon,
-        minMax: ["23°C","28°C"],
-        precipitation: "19.88mm"
-
-    },
-    {
-        day: "04",
-        weekday: "Thu",
-        Icon: CloudDrizzleIcon,
-        minMax: ["23°C","28°C"],
-        precipitation: "19.88mm"
-
-    },
-]
+	{
+		day: "01",
+		weekday: "Mon",
+		Icon: CloudDrizzleIcon,
+		minMax: ["23°C", "28°C"],
+		precipitation: "19.88mm",
+	},
+	{
+		day: "02",
+		weekday: "Tue",
+		Icon: CloudDrizzleIcon,
+		minMax: ["23°C", "28°C"],
+		precipitation: "19.88mm",
+	},
+	{
+		day: "03",
+		weekday: "Wed",
+		Icon: CloudDrizzleIcon,
+		minMax: ["23°C", "28°C"],
+		precipitation: "19.88mm",
+	},
+	{
+		day: "04",
+		weekday: "Thu",
+		Icon: CloudDrizzleIcon,
+		minMax: ["23°C", "28°C"],
+		precipitation: "19.88mm",
+	},
+];
 
 export default function DeepSchedule() {
-    
-
-    return (
-        <div className="px-4 text-icons text-xl ">
-            {mock.map((day) => {
-                return (
-                    <div key={day.day} className="grid grid-cols-4 items-center border-t border-card-foreground">
-                        <div className="text-center p-1">
-                            <p>{day.day}</p>
-                            <p>{day.weekday}</p>
-                        </div>
-                        <day.Icon size={38}/>
-                        <div className="flex gap-4">
-                            <div className="text-accent">{day.minMax[0]}</div>
-                            <div className="text-destructive">{day.minMax[1]}</div>
-                        </div>
-                        <div className="flex">
-                            <DropletIcon/>
-                            {day.precipitation}
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
-    )
+	return (
+		<div className="px-4 text-icons text-xl ">
+			{mock.map((day) => {
+				return (
+					<div
+						key={day.day}
+						className="grid grid-cols-4 items-center border-t border-card-foreground"
+					>
+						<div className="text-center p-1">
+							<p>{day.day}</p>
+							<p>{day.weekday}</p>
+						</div>
+						<day.Icon size={38} />
+						<div className="flex gap-4">
+							<div className="text-accent">{day.minMax[0]}</div>
+							<div className="text-destructive">{day.minMax[1]}</div>
+						</div>
+						<div className="flex">
+							<DropletIcon />
+							{day.precipitation}
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
---
+++ src/routes/dashboard.tsx
@@ -0,0 +1,75 @@
+import { createFileRoute } from "@tanstack/react-router";
+import dayjs from "dayjs";
+import { useState } from "react";
+import FavoriteCarousel from "@/components/FavoriteCarousel";
+import WeatherCalendar from "@/components/WeatherCalendar";
+import DeepSchedule from "@/components/DeepSchedule";
+
+export const Route = createFileRoute("/dashboard")({
+	component: RouteComponent,
+});
+
+function RouteComponent() {
+	const [dataPresentation, setDataPresentation] = useState<"simple" | "deep">("simple");
+
+	const dateData = dayjs();
+	const monthYear = dateData.format("MMMM YYYY").split(" ")
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setDataPresentation(e.target.value as "simple" | "deep");
+	};
+
+	return (
+		<div className="flex flex-col gap-2">
+			<FavoriteCarousel />
+			<div className="flex flex-col gap-6 bg-card p-4 rounded-3xl">
+				<div className="flex justify-between  items-center text-icons">
+					<h3 className="text-xl font-semibold">{`${monthYear[0]} ${monthYear[1]}`}</h3>
+					<div className="flex gap-4 border border-card-foreground rounded-3xl p-0.5">
+						<div>
+							<input
+								type="radio"
+								name="type"
+								id="simple"
+								value="simple"
+								className="sr-only peer"
+								onChange={handleChange}
+								checked={dataPresentation === "simple"}
+							/>
+							<label
+								htmlFor="simple"
+								className="peer-checked:bg-card-foreground block rounded-3xl px-6 py-0.5 cursor-pointer"
+							>
+								Simple
+							</label>
+						</div>
+
+						<div>
+							<input
+								type="radio"
+								name="type"
+								id="deep"
+								value="deep"
+								className="sr-only peer"
+								onChange={handleChange}
+								checked={dataPresentation === "deep"}
+							/>
+							<label
+								htmlFor="deep"
+								className="peer-checked:bg-card-foreground block rounded-3xl px-6 py-0.5 cursor-pointer"
+							>
+								Deep
+							</label>
+						</div>
+					</div>
+				</div>
+
+				{dataPresentation === "simple" ? (
+					<WeatherCalendar />
+				) : (
+					<DeepSchedule />
+				)}
+			</div>
+		</div>
+	);
+}
