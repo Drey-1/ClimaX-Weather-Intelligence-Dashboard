7f2400c1e9da7191f380cbf5077bda82a860bcd7
Drey-1 <matosandrey11@gmail.com>
2026-05-08 16:12:41 -0300

    fix: add return statement to loadPrincipalCity function for consistency

--- src/services/localStorage.ts
+++ src/services/localStorage.ts
@@ -6,9 +6,9 @@ const saveFavorites = (citiesList: string[]) => {
 	localStorage.setItem("favoritedCities", JSON.stringify(citiesList));
 };
 
-const loadPrincipalCity = () => {
-	localStorage.getItem("principalCity")
-}
+const loadPrincipalCity = (): string | null => {
+	return localStorage.getItem("principalCity");
+};
 
 const savePrincipalCity = (city: string) => {
 	localStorage.setItem("principalCity", city);
