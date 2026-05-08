f8f530aa83b225275a14fa73ff97b510fd9669b6
Drey-1 <matosandrey11@gmail.com>
2026-05-08 16:16:52 -0300

    feat: replace mock data with dynamic weather data in SummaryCard component

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -1,17 +1,14 @@
 import dayjs from "dayjs";
-import { CloudSunIcon, DropletsIcon, EyeIcon, MapPin, Wind } from "lucide-react";
+import { DropletsIcon, EyeIcon, MapPin, Wind } from "lucide-react";
+import { useSelectedCitys } from "@/hooks/useSelectedCity";
+import { useSummaryWeather } from "@/hooks/useSummaryWeather";
 import InfoItem from "./InfoItem";
+
 export default function SummaryCard() {
+	const { selectedCity } = useSelectedCitys();
+	const { summaryWeather } = useSummaryWeather(selectedCity);
 	const data = dayjs();
 	const today = data.format("DD MMM YY");
-	const mockCity = {
-		temperature: "24°",
-		weather: "Sunny",
-		wind: "9km/h",
-		humidity: "25%",
-		visibility: "1.2km",
-		city: "Los Angeles",
-	};
 
 	return (
 		<div className="bg-linear-to-b from-primary to-secondary p-6 rounded-3xl w-max h-max">
@@ -19,22 +16,27 @@ export default function SummaryCard() {
 				<p className=" text-gray-200">Today, {today}</p>
 				<p className="flex items-center  text-white">
 					<MapPin size={12} stroke="currentColor" />
-					{mockCity.city}
+					{selectedCity}
 				</p>
 			</div>
 			<div className="flex flex-col items-center p-4">
-				<p className="text-gray-200 text-sm">{mockCity.weather}</p>
+				<p className="text-gray-200 text-sm">{summaryWeather.text}</p>
 				<p className="text-5xl font-semibold bg-linear-to-b from-white via-white to-transparent bg-clip-text text-transparent">
-					{mockCity.temperature}
+					{summaryWeather.tempC}
 				</p>
-				<CloudSunIcon size={82} className="-translate-y-4 text-yellow-100" />
+				<img src={summaryWeather.icon} alt="" width={82} />
 			</div>
 			<div className="flex text-white text-sm items-center [&>*:not(:last-child)]:border-r [&>*:not(:last-child)]:border-white [&>*:not(:last-child)]:pr-6 [&>*:not(:first-child)]:pl-6">
-				<InfoItem icon={Wind} value={mockCity.wind} label="wind" key={"wind"} />
-				<InfoItem icon={DropletsIcon} value={mockCity.humidity} label="humidity" key={"humidity"} />
+				<InfoItem icon={Wind} value={summaryWeather.wind} label="wind" key={"wind"} />
+				<InfoItem
+					icon={DropletsIcon}
+					value={summaryWeather.humidity}
+					label="humidity"
+					key={"humidity"}
+				/>
 				<InfoItem
 					icon={EyeIcon}
-					value={mockCity.visibility}
+					value={summaryWeather.visibility}
 					label="visibility"
 					key={"visibility"}
 				/>
