8041c9ae1f7ac7f220db53157689167bada9dd63
Drey-1 <matosandrey11@gmail.com>
2026-05-09 15:21:47 -0300

    feat: replace mock data with dynamic weather data in ForecastCard component

--- src/components/ForecastCard.tsx
+++ src/components/ForecastCard.tsx
@@ -1,72 +1,24 @@
-import { Cloud } from "lucide-react";
+import { useNext7DaysWeather } from "@/hooks/useNext7DaysWeather";
+import { useSelectedCitys } from "@/hooks/useSelectedCity";
 
 export default function ForecastCard() {
-	const mockCitys = [
-		{
-			weekday: 0,
-			icon: null,
-			weather: "windy",
-			minTemp: "16°",
-			maxTemp: "28°",
-		},
-		{
-			weekday: 1,
-			icon: null,
-			weather: "windy",
-			minTemp: "16°",
-			maxTemp: "28°",
-		},
-		{
-			weekday: 2,
-			icon: null,
-			weather: "windy",
-			minTemp: "16°",
-			maxTemp: "28°",
-		},
-		{
-			weekday: 3,
-			icon: null,
-			weather: "windy",
-			minTemp: "16°",
-			maxTemp: "28°",
-		},
-		{
-			weekday: 4,
-			icon: null,
-			weather: "windy",
-			minTemp: "16°",
-			maxTemp: "28°",
-		},
-		{
-			weekday: 5,
-			icon: null,
-			weather: "Cloudy",
-			minTemp: "16°",
-			maxTemp: "28°",
-		},
-		{
-			weekday: 6,
-			icon: null,
-			weather: "windy",
-			minTemp: "16°",
-			maxTemp: "28°",
-		},
-	];
-	const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	const { selectedCity } = useSelectedCitys();
+	const { next7DaysWeather } = useNext7DaysWeather(selectedCity);
+	console.log(next7DaysWeather[1])
 	return (
 		<div className="relative bg-card rounded-3xl p-4 w-full h-82 flex flex-col">
 			<h2 className="text-icons font-semibold text-xl">Next 7 days</h2>
 			<div className="flex-1 overflow-y-scroll scrollbar-hide">
-				{mockCitys.map((city) => {
+				{next7DaysWeather.map((city) => {
 					return (
-						<div key={city.weekday} className="grid grid-cols-3 items-center text-icons p-4">
-							<p>{weekdays[city.weekday]}</p>
-							<div className="flex gap-2 justify-center">
-								{city.icon ?? <Cloud />}
-								<p>{city.weather}</p>
+						<div key={city.dayOfWeek} className="grid grid-cols-3 items-center text-icons p-4">
+							<p>{city.dayOfWeek}</p>
+							<div className="flex gap-2 justify-center items-center">
+								<img src={city.icon} alt="" />
+								<p>{city.text}</p>
 							</div>
 							<p className="text-right tabular-nums">
-								{city.minTemp}/{city.maxTemp}
+								{city.minTempC}/{city.maxTempC}
 							</p>
 						</div>
 					);
@@ -75,11 +27,13 @@ export default function ForecastCard() {
 					<div className="absolute -top-12 pointer-events-none bg-linear-to-b from-transparent to-card w-full h-12"></div>
 					<div className="grid grid-cols-3 items-center p-4 bg-linear-to-b from-primary to-secondary rounded-2xl text-white">
 						<p className="">Tommorow</p>
-						<div className="flex gap-2 justify-center">
-							<Cloud />
-							<p>Cloudy</p>
+						<div className="flex gap-2 justify-center items-center">
+							<img src={next7DaysWeather[1]?.icon} alt="" />
+							<p>{next7DaysWeather[1]?.text}</p>
 						</div>
-						<p className="text-right tabular-nums">16°/23°</p>
+						<p className="text-right tabular-nums">
+							{next7DaysWeather[1]?.minTempC}/{next7DaysWeather[1]?.maxTempC}
+						</p>
 					</div>
 				</div>
 			</div>
