626a07effe1b1d1186b696b36650c924b1a6a82d
Drey-1 <matosandrey11@gmail.com>
2026-05-16 16:54:09 -0300

    feat: wrap RouterProvider with SelectedCityProvider for global state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import { createRouter, RouterProvider } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen.ts'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { SelectedCityProvider } from "./contexts/SelectedCityContext.tsx";
+import { routeTree } from "./routeTree.gen.ts";
 
-const router = createRouter({routeTree})
+const router = createRouter({ routeTree });
 
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
 }
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<SelectedCityProvider>
+			<RouterProvider router={router} />
+		</SelectedCityProvider>
+	</StrictMode>,
+);
