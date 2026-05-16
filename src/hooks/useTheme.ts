8138a671467dc97cc7f665d34ddce63b9ccb977f
Drey-1 <matosandrey11@gmail.com>
2026-05-16 17:02:02 -0300

    feat: initialize theme state based on local storage and update saveTheme logic

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
+import { loadTheme, saveTheme } from "@/services/localStorage";
 
 export function useTheme() {
-	const [isDarkMode, setDarkMode] = useState(false);
+	const [isDarkMode, setDarkMode] = useState(() => {
+		const theme = loadTheme();
+		return theme === "dark";
+	});
 
 	const toggleTheme = () => {
 		setDarkMode(!isDarkMode);
@@ -9,6 +13,7 @@ export function useTheme() {
 
 	useEffect(() => {
 		document.documentElement.classList.toggle("dark", isDarkMode);
+		saveTheme(isDarkMode ? "dark" : "light");
 	}, [isDarkMode]);
 
 	return { isDarkMode, toggleTheme };
