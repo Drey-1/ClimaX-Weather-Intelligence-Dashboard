7abf5d8b4c05d93c5b00b3fb1d179aeb97f85122
Drey-1 <matosandrey11@gmail.com>
2026-05-20 15:57:25 -0300

    feat: wrap RouterProvider with FavoritesCitiesProvider for context management

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { FavoritesCitiesProvider } from "./contexts/FavoritesContext.tsx";
 import { SelectedCityProvider } from "./contexts/SelectedCityContext.tsx";
 import { routeTree } from "./routeTree.gen.ts";
 
@@ -16,7 +17,9 @@ declare module "@tanstack/react-router" {
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<SelectedCityProvider>
-			<RouterProvider router={router} />
+			<FavoritesCitiesProvider>
+				<RouterProvider router={router} />
+			</FavoritesCitiesProvider>
 		</SelectedCityProvider>
 	</StrictMode>,
 );
