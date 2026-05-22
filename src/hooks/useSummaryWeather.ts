7a28c6c387e2daa67ac858f2670ce7be4e77fc7c
Drey-1 <matosandrey11@gmail.com>
2026-05-22 17:00:50 -0300

    refactor: replace useEffect with useQuery for fetching weather data for better state handling

--- src/hooks/useSummaryWeather.ts
+++ src/hooks/useSummaryWeather.ts
@@ -1,31 +1,22 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { getCurrent } from "@/services/wheatherService";
 
+
 export const useSummaryWeather = (selectedCity: string) => {
-	const [summaryWeather, setSummaryWeather] = useState({});
-
-	useEffect(() => {
-		if (!selectedCity) return;
-		const fetchData = async () => {
-			try {
-				const data = await getCurrent(selectedCity);
-				const usefulData = {
-					icon: data.current.condition.icon,
-					tempC: data.current.temp_c.toFixed(0) + "°",
-					text: data.current.condition.text,
-					wind: data.current.wind_kph + "km/h",
-					humidity: data.current.humidity + "%",
-					visibility: data.current.vis_km + "km",
-				};
-				console.log(usefulData);
-				setSummaryWeather(usefulData);
-				console.log(data);
-			} catch (err) {
-				console.error(err);
-			}
-		};
-		fetchData();
-	}, [selectedCity]);
-
-	return { summaryWeather };
+	return useQuery({
+		queryKey: ["summaryWeather", selectedCity],
+		queryFn: async () => {
+			const data = await getCurrent(selectedCity);
+			
+			return {
+				icon: data.current.condition.icon,
+				tempC: data.current.temp_c.toFixed(0) + "°",
+				text: data.current.condition.text,
+				wind: data.current.wind_kph + "km/h",
+				humidity: data.current.humidity + "%",
+				visibility: data.current.vis_km + "km",
+			};
+		},
+		enabled: !!selectedCity,
+	});
 };
