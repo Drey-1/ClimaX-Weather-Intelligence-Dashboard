d1fbe74027edafe7c2a29768555e0790619ea2d8
Drey-1 <matosandrey11@gmail.com>
2026-05-29 16:12:59 -0300

    refactor: replace useEffect with useQuery for fetching deep forecasts

--- src/hooks/useDeepForecast.ts
+++ src/hooks/useDeepForecast.ts
@@ -1,37 +1,26 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { getForecast } from "@/services/wheatherService";
 import type { deepCityForecastType } from "@/types/citiesWeathersType";
 
 export const useDeepForecast = (selectedCity: string | null) => {
-	const [deepForecasts, setDeepForescasts] = useState([]);
-
-	useEffect(() => {
-		if (!selectedCity) return;
-		const fetchData = async () => {
-			try {
-				const data = await getForecast(selectedCity, 14);
-				const usefulData = data.forecast.forecastday.map((item: deepCityForecastType) => {
-					const dateChar = item.date;
-					const date = new Date(`${dateChar}T00:00:00`);
-                    
-					return {
-						day: dateChar.split("-")[2],
-						icon: item.day.condition.icon,
-						dayOfWeek: date.toLocaleDateString("en-US", { weekday: "short" }),
-						minTempC: item.day.mintemp_c.toFixed(0) + "°C",
-						maxTempC: item.day.maxtemp_c.toFixed(0) + "°C",
-						precipitation: item.day.totalprecip_mm.toFixed(2) + "mm",
-					};
-				});
-				console.log(usefulData);
-				setDeepForescasts(usefulData);
-				console.log(data);
-			} catch (err) {
-				console.error(err);
-			}
-		};
-		fetchData();
-	}, [selectedCity]);
-
-	return { deepForecasts };
+	return useQuery({
+		queryKey: ["deepForecast", selectedCity],
+		queryFn: async () => {
+			const data = await getForecast(selectedCity as string, 14);
+			return data.forecast.forecastday.map((item: deepCityForecastType) => {
+				const dateChar = item.date;
+				const date = new Date(`${dateChar}T00:00:00`);
+				return {
+					day: dateChar.split("-")[2],
+					icon: item.day.condition.icon,
+					dayOfWeek: date.toLocaleDateString("en-US", { weekday: "short" }),
+					minTempC: item.day.mintemp_c.toFixed(0) + "°C",
+					maxTempC: item.day.maxtemp_c.toFixed(0) + "°C",
+					precipitation: item.day.totalprecip_mm.toFixed(2) + "mm",
+				};
+				
+			});
+		},
+		enabled: !!selectedCity,
+	});
 };
