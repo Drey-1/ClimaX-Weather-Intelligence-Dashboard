d6545791cb2822508e779b0105f2e6dde850308f
Drey-1 <matosandrey11@gmail.com>
2026-05-29 17:00:00 -0300

    refactor: replace useEffect with useQuery in useWeatherCalendar hook

--- src/hooks/useSummaryWeather.ts
+++ src/hooks/useSummaryWeather.ts
@@ -1,13 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCurrent } from "@/services/wheatherService";
 
-
 export const useSummaryWeather = (selectedCity: string) => {
 	return useQuery({
 		queryKey: ["summaryWeather", selectedCity],
 		queryFn: async () => {
 			const data = await getCurrent(selectedCity);
-			
+
 			return {
 				icon: data.current.condition.icon,
 				tempC: data.current.temp_c.toFixed(0) + "°",
--- src/hooks/useWeatherCalendar.ts
+++ src/hooks/useWeatherCalendar.ts
@@ -1,29 +1,19 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { getForecast } from "@/services/wheatherService";
 import type { calendarCityForecastType } from "@/types/citiesWeathersType";
 
 export const useWeatherCalendar = (selectedCity: string | null) => {
-	const [calendarIcons, setCalendarIcons] = useState([]);
-
-	useEffect(() => {
-		if (!selectedCity) return;
-		const fetchData = async () => {
-			try {
-				const data = await getForecast(selectedCity, 14);
-				const usefulData = data.forecast.forecastday.map((item: calendarCityForecastType) => {
-					return {
-						icon: item.day.condition.icon,
-						date: item.date,
-					};
-				});
-				console.log(usefulData);
-				setCalendarIcons(usefulData);
-			} catch (err) {
-				console.error(err);
-			}
-		};
-		fetchData();
-	}, [selectedCity]);
-
-	return { calendarIcons };
+	return useQuery({
+		queryKey: ["caledarIcons", selectedCity],
+		queryFn: async () => {
+			const data = await getForecast(selectedCity as string, 14);
+			return data.forecast.forecastday.map((item: calendarCityForecastType) => {
+				return {
+					icon: item.day.condition.icon,
+					date: item.date,
+				};
+			});
+		},
+		enabled: !!selectedCity,
+	});
 };
