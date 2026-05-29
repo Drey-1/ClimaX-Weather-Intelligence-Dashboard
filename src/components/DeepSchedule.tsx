5101933a8ea3f127148afec23b7a24140c3fffa2
Drey-1 <matosandrey11@gmail.com>
2026-05-29 16:13:15 -0300

    fix: add loading and error handling to DeepSchedule component

--- src/components/DeepSchedule.tsx
+++ src/components/DeepSchedule.tsx
@@ -4,7 +4,25 @@ import { useDeepForecast } from "@/hooks/useDeepForecast";
 
 export default function DeepSchedule() {
 	const { selectedCity } = useSelectedFavoriteCity();
-	const { deepForecasts } = useDeepForecast(selectedCity);
+	const { data: deepForecasts, isPending, isError } = useDeepForecast(selectedCity);
+	console.log(deepForecasts)
+
+	if (isPending) return (
+    <div className="flex flex-col gap-2">
+        {Array.from({ length: 7 }).map((_, i) => (
+            <div className="grid grid-cols-4 gap-4 px-2" key={i}>
+			<div className="h-13 bg-icons animate-pulse rounded-2xl"></div>
+			<div className="h-13 bg-icons animate-pulse rounded-2xl col-span-2"></div>
+			<div className="h-13 bg-icons animate-pulse rounded-2xl"></div>
+		</div>
+        ))}
+    </div>
+);
+    if (isError) return (
+		<div className="bg-destructive  p-6 rounded-3xl w-max h-max">
+				<p className="text-white text-sm">Error loading data.</p>
+			</div>
+	);
 
 	return (
 		<div className="px-4 text-icons text-xl ">
