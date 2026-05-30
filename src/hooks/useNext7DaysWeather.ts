9a901f3a6aa5e0c4751517b29c03a52619b2b1f0
Drey-1 <matosandrey11@gmail.com>
2026-05-30 15:39:28 -0300

    refactor: replace useEffect and useState with useQuery in useNext7DaysWeather hook

--- src/hooks/useNext7DaysWeather.ts
+++ src/hooks/useNext7DaysWeather.ts
@@ -1,35 +1,23 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { getForecast } from "@/services/wheatherService";
 
 export const useNext7DaysWeather = (selectedCity: string) => {
-	const [next7DaysWeather, setNext7DaysWeather] = useState([]);
-
-	useEffect(() => {
-		if (!selectedCity) return;
-		const fetchData = async () => {
-			try {
-				const data = await getForecast(selectedCity, 7);
-				const usefulData = data.forecast.forecastday.map((item) => {
-					const dateChar = item.date;
-					const date = new Date(`${dateChar}T00:00:00`);
-
-					return {
-                        text: item.day.condition.text,
-						icon: item.day.condition.icon,
-						dayOfWeek: date.toLocaleDateString("en-US", { weekday: "long" }),
-						minTempC: item.day.mintemp_c.toFixed(0) + "°C",
-						maxTempC: item.day.maxtemp_c.toFixed(0) + "°C",
-					};
-				});
-				console.log(usefulData);
-				setNext7DaysWeather(usefulData);
-				console.log(data);
-			} catch (err) {
-				console.error(err);
-			}
-		};
-		fetchData();
-	}, [selectedCity]);
-
-	return { next7DaysWeather };
+	return useQuery({
+		queryKey: ["next7DaysWeather", selectedCity],
+		queryFn: async () => {
+			const data = await getForecast(selectedCity, 7);
+			return data.forecast.forecastday.map((item) => {
+				const dateChar = item.date;
+				const date = new Date(`${dateChar}T00:00:00`);
+				return {
+					text: item.day.condition.text,
+					icon: item.day.condition.icon,
+					dayOfWeek: date.toLocaleDateString("en-US", { weekday: "long" }),
+					minTempC: item.day.mintemp_c.toFixed(0) + "°C",
+					maxTempC: item.day.maxtemp_c.toFixed(0) + "°C",
+				};
+			});
+		},
+		enabled: !!selectedCity
+	});
 };
