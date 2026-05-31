dc655694fb42b2aa6c029d94644b383f5bfc0554
Drey-1 <matosandrey11@gmail.com>
2026-05-31 16:46:13 -0300

    refactor: replace useEffect and useState with useQuery in useFavoritesWeather hook

--- src/hooks/useFavoritesWeather.ts
+++ src/hooks/useFavoritesWeather.ts
@@ -1,34 +1,23 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { getForecast } from "@/services/wheatherService";
-import type { citiesWeathersType } from "@/types/citiesWeathersType";
 
 export const useFavoritesWeather = (favorites: string[]) => {
-	const [citiesWeathers, setCitiesWeathers] = useState<citiesWeathersType[]>([]);
-
-	useEffect(() => {
-		if (!favorites.length) return;
-		const fetchCitiesData = async () => {
-			try {
-				const cityDatas: citiesWeathersType[] = await Promise.all(
-					favorites.map(async (city) => {
-						const cityData = await getForecast(city, 1);
-						return {
-							icon: cityData.current.condition.icon,
-							name: cityData.location.name,
-							tempC: cityData.current.temp_c.toFixed(0),
-							maxTempC: cityData.forecast.forecastday[0].day.maxtemp_c.toFixed(0),
-							minTempC: cityData.forecast.forecastday[0].day.mintemp_c.toFixed(0),
-						};
-					}),
-				);
-				console.log(cityDatas);
-				setCitiesWeathers(cityDatas);
-			} catch (err) {
-				console.error(err);
-			}
-		};
-		fetchCitiesData();
-	}, [favorites]);
-
-	return { citiesWeathers };
+	return useQuery({
+		queryKey: ["citiesWeathers", favorites],
+		queryFn: async () => {
+			return await Promise.all(
+				favorites.map(async (city) => {
+					const cityData = await getForecast(city, 1);
+					return {
+						icon: cityData.current.condition.icon,
+						name: cityData.location.name,
+						tempC: cityData.current.temp_c.toFixed(0),
+						maxTempC: cityData.forecast.forecastday[0].day.maxtemp_c.toFixed(0),
+						minTempC: cityData.forecast.forecastday[0].day.mintemp_c.toFixed(0),
+					};
+				}),
+			);
+		},
+		enabled: favorites.length > 0,
+	});
 };
