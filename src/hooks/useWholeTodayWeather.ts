7bb8ef38bb3b26ac06b67cf247b31d74930f593a
Drey-1 <matosandrey11@gmail.com>
2026-05-31 15:09:15 -0300

    refactor: replace useEffect and useState with useQuery in useWholeTodayWeather hook

--- src/hooks/useWholeTodayWeather.ts
+++ src/hooks/useWholeTodayWeather.ts
@@ -1,31 +1,20 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { getForecast } from "@/services/wheatherService";
 
 export const useWholeTodayWeather = (selectedCity: string) => {
-	const [wholeTodayWeather, setWholeTodayWeather] = useState([]);
-
-	useEffect(() => {
-		if (!selectedCity) return;
-		const fetchData = async () => {
-			try {
-				const data = await getForecast(selectedCity, 1);
-				const usefulData = data.forecast.forecastday[0].hour.map((item) => {
-					return {
-						time: item.time.split(" ")[1],
-						tempC: item.temp_c.toFixed(0),
-						humidity: item.humidity,
-						rainfall: item.precip_mm,
-					};
-				});
-				console.log(usefulData);
-				setWholeTodayWeather(usefulData);
-				console.log(data);
-			} catch (err) {
-				console.error(err);
-			}
-		};
-		fetchData();
-	}, [selectedCity]);
-
-	return { wholeTodayWeather };
+	return useQuery({
+		queryKey: ["wholeTodayWeather", selectedCity],
+		queryFn: async () => {
+			const data = await getForecast(selectedCity, 1);
+			return data.forecast.forecastday[0].hour.map((item) => {
+				return {
+					time: item.time.split(" ")[1],
+					tempC: item.temp_c.toFixed(0),
+					humidity: item.humidity,
+					rainfall: item.precip_mm,
+				};
+			});
+		},
+		enabled: !!selectedCity,
+	});
 };
