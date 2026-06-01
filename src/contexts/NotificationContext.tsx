58c261740eeb6e8f15677ea6f51ad002cd58da7e
Drey-1 <matosandrey11@gmail.com>
2026-06-01 17:00:19 -0300

    feat: implement NotificationContext for managing notifications

---
+++ src/contexts/NotificationContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+import type { NotificationContextType, NotificationType } from "@/types/NotificationContextTypes";
+
+const NOTIFICATION_DURATION_MS = 4000;
+
+const NotificationContext = createContext<NotificationContextType>({} as NotificationContextType);
+
+export const NotificationProvider = ({ children }: { children: React.ReactNode }) => {
+	const [notifications, setNotifications] = useState<NotificationType[]>([]);
+
+	const notification = (message: string) => {
+		const id = Date.now();
+		setNotifications((prev) => [...prev, { id, message }]);
+		setTimeout(
+			() => setNotifications((prev) => prev.filter((notification) => notification.id !== id)),
+			NOTIFICATION_DURATION_MS,
+		);
+	};
+
+	return (
+		<NotificationContext.Provider value={{ notification }}>
+			{children}
+			<div className="fixed top-20 right-5 flex flex-col-reverse gap-2">
+				{notifications.map((notification) => (
+					<div
+						key={notification.id}
+						className="bg-card border border-card-foreground rounded-xl px-5 py-4 w-72 shadow-sm relative overflow-hidden animate-slide-in"
+					>
+						<h1 className="text-sm font-medium text-icons mb-1">Notification</h1>
+						<p className="text-xs text-card-foreground">{notification.message}</p>
+						<div className="absolute bottom-0 left-0 h-0.5 bg-gray-200 animate-shrink" />
+					</div>
+				))}
+			</div>
+		</NotificationContext.Provider>
+	);
+};
+
+export const useNotification = () => useContext(NotificationContext);
