f835309e5ddcd79d883b38aa9c6a4b68cca883bf
Drey-1 <matosandrey11@gmail.com>
2026-06-02 15:50:48 -0300

    feat: integrate notification system for city selection and favorite toggle

--- src/components/SearchDialog.tsx
+++ src/components/SearchDialog.tsx
@@ -1,5 +1,6 @@
 import { MapPin, Star } from "lucide-react";
 import { useFavorites } from "@/contexts/FavoritesContext";
+import { useNotification } from "@/contexts/NotificationContext";
 import { useSelectedCity } from "@/contexts/SelectedCityContext";
 import { useCitySearch } from "@/hooks/useCitySearch";
 import { DialogClose, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
@@ -8,6 +9,7 @@ export default function SearchDialog() {
 	const { query, setQuery, cityList, isPending, isError } = useCitySearch();
 	const { isFavorited, toggleFavorite } = useFavorites();
 	const { changeSelectedCity } = useSelectedCity();
+	const { notification } = useNotification();
 
 	return (
 		<DialogContent showCloseButton={false} className="border border-card">
@@ -35,9 +37,7 @@ export default function SearchDialog() {
 			</DialogHeader>
 			<div className="p-2">
 				<h3 className="text-sm p-2">Suggestions</h3>
-				{isPending && (
-						<p className="text-icons text-xl ">Loading...</p>
-				)}
+				{isPending && <p className="text-icons text-xl ">Loading...</p>}
 				{isError && (
 					<div className="bg-destructive  p-6 rounded-3xl w-max h-max">
 						<p className="text-white text-lg">Error loading data.</p>
@@ -48,14 +48,23 @@ export default function SearchDialog() {
 						<div
 							key={item.id}
 							className="flex p-2 m-1 justify-between hover:bg-select group rounded-xl cursor-pointer"
-							onClick={() => changeSelectedCity(item.city)}
+							onClick={() => {
+								changeSelectedCity(item.city);
+								notification(`Main city changed to ${item.city}`);
+							}}
 						>
 							<div className="flex gap-1 cursor-pointer">
 								<MapPin className="text-card-foreground group-hover:-rotate-90 transition-transform group-hover:scale-115" />
 								<p className="text-icons">{item.city}</p>
 							</div>
 							<Star
-								onClick={() => toggleFavorite(item.city)}
+								onClick={(e) => {
+									e.stopPropagation();
+									toggleFavorite(item.city);
+									notification(
+										`${item.city} ${isFavorited(item.city) ? "was removed from your favorites list" : "has been added to your favorites list"}`,
+									);
+								}}
 								fill={isFavorited(item.city) ? "currentColor" : "none"}
 								stroke="currentColor"
 								className={"text-icons cursor-pointer"}
