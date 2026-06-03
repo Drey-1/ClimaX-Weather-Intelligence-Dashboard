0cb3991a4eb07df50a35ed77f9f53da51a966efe
Drey-1 <matosandrey11@gmail.com>
2026-06-03 16:18:19 -0300

    feat: add responsive image sizing in ForecastCard component

--- src/components/ForecastCard.tsx
+++ src/components/ForecastCard.tsx
@@ -46,7 +46,7 @@ export default function ForecastCard() {
 						<div key={city.dayOfWeek} className="grid grid-cols-3 items-center text-icons p-4">
 							<p>{city.dayOfWeek}</p>
 							<div className="flex gap-2 justify-center items-center">
-								<img src={city.icon} alt="" />
+								<img src={city.icon} alt="" className="w-9 md:w-16" />
 								<p>{city.text}</p>
 							</div>
 							<p className="text-right tabular-nums">
@@ -60,7 +60,7 @@ export default function ForecastCard() {
 					<div className="grid grid-cols-3 items-center p-4 bg-linear-to-b from-primary to-secondary rounded-2xl text-white">
 						<p className="">Tommorow</p>
 						<div className="flex gap-2 justify-center items-center">
-							<img src={next7DaysWeather[1]?.icon} alt="" />
+							<img src={next7DaysWeather[1]?.icon} alt="" className="w-9 md:w-16" />
 							<p>{next7DaysWeather[1]?.text}</p>
 						</div>
 						<p className="text-right tabular-nums">
