52e897069db3d0a2324c2a5cd68f84546df547a1
Drey-1 <matosandrey11@gmail.com>
2026-06-03 16:18:40 -0300

    feat: update layout in RouteComponent for responsive design

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,7 @@ export const Route = createFileRoute("/")({
 function RouteComponent() {
 	return (
 		<div className="flex flex-col gap-4">
-			<div className="flex gap-4 mt-2">
+			<div className="flex flex-col md:flex-row gap-4 mt-2">
 				<SummaryCard />
 				<ForecastCard />
 			</div>
