f4026bada3d0451b06d2f9a74c733b25e8c906e1
Drey-1 <matosandrey11@gmail.com>
2026-06-03 16:17:50 -0300

    feat: improve formatting and spacing in SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,20 @@
-import { Search, } from "lucide-react";
-import {
-	Dialog,
-	DialogTrigger,
-} from "@/components/ui/dialog";
+import { Search } from "lucide-react";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import SearchDialog from "./SearchDialog";
 
 export default function SearchBar() {
 	return (
 		<Dialog>
 			<DialogTrigger asChild>
-				<button type="button" className="flex p-2 gap-1 rounded-full w-lg bg-card cursor-pointer">
+				<button
+					type="button"
+					className="flex p-2 gap-1 rounded-full w-84  sm:w-lg bg-card cursor-pointer"
+				>
 					<Search className="text-icons" />
 					<p className="text-card-foreground">Search City...</p>
 				</button>
 			</DialogTrigger>
-			<SearchDialog/>
+			<SearchDialog />
 		</Dialog>
 	);
 }
