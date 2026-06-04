16927ea42be7208b26edd93875caa18e153272b9
Drey-1 <matosandrey11@gmail.com>
2026-06-04 16:27:50 -0300

    feat: enhance day display and layout in WeatherCalendar component for better responsiveness

--- src/components/WeatherCalendar.tsx
+++ src/components/WeatherCalendar.tsx
@@ -30,7 +30,8 @@ export default function WeatherCalendar() {
 			<div className="grid grid-cols-7 border-b bg-primary">
 				{WEEK_DAYS.map((day) => (
 					<div key={day} className="p-3 text-center text-sm text-icons border-r last:border-r-0">
-						{day}
+						<span className="inline sm:hidden">{day.slice(0, 3)}</span>
+						<span className="hidden sm:inline">{day}</span>
 					</div>
 				))}
 			</div>
@@ -45,21 +46,29 @@ export default function WeatherCalendar() {
 					return (
 						<div
 							key={i}
-							className={`h-32 p-2 border-r border-b relative flex flex-col items-end group hover:scale-110 hover:z-10 transition-transform
+							className={`h-12 md:h-32 p-2 border-r border-b relative flex flex-col items-end group hover:scale-110 hover:z-10 transition-transform
                 ${!isCurrentMonth ? "bg-foreground text-card-foreground" : "text-icons hover:bg-card"} ${isToday ? "bg-linear-to-b from-primary to-secondary" : ""}
               `}
 						>
 							<div className="flex gap-2 justify-between items-center text-sm w-full">
-								{isToday && <span className="font-bold text-white text-xs uppercase">Today</span>}
-								{isSameDay(day, tomorrow) && (
-									<span className="font-bold text-xs uppercase text-accent">Tomorrow</span>
+								{isToday && (
+									<span className="font-bold text-white text-xs uppercase hidden sm:inline">
+										Today
+									</span>
 								)}
-								<span className={`absolute top-2 right-2 ${isToday ? "font-bold text-white" : ""}`}>
+								{isSameDay(day, tomorrow) && (
+									<span className="font-bold text-xs uppercase text-accent hidden sm:inline">
+										Tomorrow
+									</span>
+								)}
+								<span
+									className={`absolute top-1 right-1 sm:top-2 sm:right-2 ${isToday ? "font-bold text-white" : ""}`}
+								>
 									{format(day, "dd")}
 								</span>
 							</div>
 
-							<div className="flex-1 w-full flex items-center justify-center">
+							<div className="flex-1 w-full flex items-end sm:items-center justify-center">
 								{forecast && <img src={forecast.icon} alt="" />}
 							</div>
 						</div>
