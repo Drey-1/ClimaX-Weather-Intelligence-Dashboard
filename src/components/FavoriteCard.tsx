c9d7607bf0ab6d3b99ae6214a904e3444519d9e8
Drey-1 <matosandrey11@gmail.com>
2026-06-04 15:21:48 -0300

    feat: adjust padding and improve layout structure in FavoriteCard component

--- src/components/FavoriteCard.tsx
+++ src/components/FavoriteCard.tsx
@@ -14,7 +14,7 @@ export default function FavoriteCards({
 	const { notification } = useNotification();
 
 	return (
-		<div className="grid grid-cols-5 items-center border-2 border-card rounded-3xl p-4 justify-between ">
+		<div className="grid grid-cols-5 items-center border-2 border-card rounded-3xl p-2 sm:p-4 justify-between ">
 			<button
 				type="button"
 				onClick={() => {
@@ -24,17 +24,21 @@ export default function FavoriteCards({
 			>
 				<StarIcon size={38} fill="currentColor" className="text-icons cursor-pointer" />
 			</button>
-			<img src={icon} alt="" />
-			<p className="text-icons text-3xl">{name}</p>
-			<p className="text-icons text-3xl">{tempC}°C</p>
-			<div className="grid grid-cols-2 border-l border-card-foreground text-icons pl-12 pr-4 gap-10">
-				<div className="flex flex-col text-accent text-center">
-					<h3>Min</h3>
-					<p>{minTempC}°C</p>
-				</div>
-				<div className="flex flex-col text-destructive text-center">
-					<h3>Max</h3>
-					<p>{maxTempC}°C</p>
+			<div className="flex col-span-2 flex-col sm:flex-row justify-around items-center">
+				<img src={icon} alt="" />
+				<p className="text-icons text-lg sm:text-3xl">{name}</p>
+			</div>
+			<div className="flex col-span-2 flex-col sm:flex-row justify-around items-center">
+				<p className="text-icons text-lg sm:text-3xl">{tempC}°C</p>
+				<div className="grid grid-cols-2 border-l border-card-foreground text-icons pl-4 sm:pl-12 pr-4 gap-10">
+					<div className="flex flex-col text-accent text-center">
+						<h3>Min</h3>
+						<p>{minTempC}°C</p>
+					</div>
+					<div className="flex flex-col text-destructive text-center">
+						<h3>Max</h3>
+						<p>{maxTempC}°C</p>
+					</div>
 				</div>
 			</div>
 		</div>
