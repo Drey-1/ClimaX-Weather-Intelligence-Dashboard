758b9571d08aec430890fb4f8a73ff5899c97928
Drey-1 <matosandrey11@gmail.com>
2026-06-06 15:28:26 -0300

    feat: add type annotation for item in useWholeTodayWeather hook

--- src/hooks/useWholeTodayWeather.ts
+++ src/hooks/useWholeTodayWeather.ts
@@ -1,12 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getForecast } from "@/services/wheatherService";
+import type { receivedWholeTodayWeatherType } from "@/types/wholeTodayWeatherType";
 
 export const useWholeTodayWeather = (selectedCity: string) => {
 	return useQuery({
 		queryKey: ["wholeTodayWeather", selectedCity],
 		queryFn: async () => {
 			const data = await getForecast(selectedCity, 1);
-			return data.forecast.forecastday[0].hour.map((item) => {
+			return data.forecast.forecastday[0].hour.map((item: receivedWholeTodayWeatherType) => {
 				return {
 					time: item.time.split(" ")[1],
 					tempC: item.temp_c.toFixed(0),
